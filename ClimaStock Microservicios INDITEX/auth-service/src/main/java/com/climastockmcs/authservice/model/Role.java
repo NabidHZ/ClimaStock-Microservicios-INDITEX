35d30f498c312d8a6d825727f72c9068cb833a53
NabidHZ <nabidhassan34@gmail.com>
2025-10-08 22:27:33 +0200

    roles y usuarioa creados

---
+++ ClimaStock Microservicios INDITEX/auth-service/src/main/java/com/climastockmcs/authservice/model/Role.java
@@ -0,0 +1,29 @@
+package com.climastockmcs.authservice.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+
+
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false, unique=true)
+    private String name;
+
+
+}
---
+++ ClimaStock Microservicios INDITEX/auth-service/src/main/java/com/climastockmcs/authservice/model/User.java
@@ -0,0 +1,33 @@
+package com.climastockmcs.authservice.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Set;
+import java.util.HashSet;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable=false, unique=true)
+    private  String username;
+
+    @Column(nullable=false)
+    private String email;
+
+    @Column(nullable=false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Set<Role> roles = new HashSet<>();
+
+}
