fc808c74c543a12a318dab1f5db707d16e230175
NabidHZ <nabidhassan34@gmail.com>
2025-10-10 21:42:05 +0200

    fronted creado

--- ClimaStock Microservicios INDITEX/auth-service/src/main/java/com/climastockmcs/authservice/config/DataInitializer.java
+++ ClimaStock Microservicios INDITEX/auth-service/src/main/java/com/climastockmcs/authservice/config/DataInitializer.java
@@ -10,12 +10,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DataInitializer {
     @Bean
-    public CommandLineRunner initRoles(RolRepository rolRepository) {
+    public CommandLineRunner initRoles(RolRepository rolRepository) { //ComandLineRunner es una interfaz funcional que se utiliza para ejecutar código al iniciar la aplicación.
         return args -> {
-            if (rolRepository.findByName("ROLE_USER").isEmpty()) {
+            if (rolRepository.findByName("ROLE_USER").isEmpty()) { //Si no existe el rol "ROLE_USER", lo crea y lo guarda en la base de datos.
                 rolRepository.save(new Role(null, "ROLE_USER"));
             }
-            if (rolRepository.findByName("ROLE_ADMIN").isEmpty()) {
+            if (rolRepository.findByName("ROLE_ADMIN").isEmpty()) { //Si no existe el rol "ROLE_ADMIN", lo crea y lo guarda en la base de datos.
                 rolRepository.save(new Role(null, "ROLE_ADMIN"));
             }
         };
